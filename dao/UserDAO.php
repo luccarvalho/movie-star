af89a2887735e788b0d05baa8a30807675721d8b
Lucas <lucasalexandre_82@hotmail.com>
2022-11-26 13:09:06 -0300

    Criando registro de usuário no Banco de Dados.

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -1,16 +1,19 @@
 <?php
 
 require_once("models/User.php");
+require_once("models/Message.php");
 
 class UserDAO implements UserDAOInterface
 {
     private $conn;
     private $url;
+    private $message;
 
     public function __construct(PDO $conn, $url)
     {
         $this->conn = $conn;
         $this->url = $url;
+        $this->message = new Message($url);
     }
 
     public function buildUser($data)
@@ -31,19 +34,46 @@ class UserDAO implements UserDAOInterface
 
     public function create(User $user, $authUser = false)
     {
+        $stmt = $this->conn->prepare("INSERT INTO users(name, lastname, email, password, token)
+            VALUES (:name, :lastname, :email, :password, :token)");
+
+        $stmt->bindParam(":name", $user->name);
+        $stmt->bindParam(":lastname", $user->lastname);
+        $stmt->bindParam(":email", $user->email);
+        $stmt->bindParam(":password", $user->password);
+        $stmt->bindParam(":token", $user->token);
+
+        $stmt->execute();
+
+        // Autenticar usuário, caso o auth seja true
+        if($authUser) {
+            $this->setTokenToSession($user->token);
+        }
     }
+    
     public function update(User $user)
     {
     }
+    
     public function verifyToken($protected = false)
     {
     }
+    
     public function setTokenToSession($token, $redirect = true)
     {
+        // Salvar token na sessão
+        $_SESSION["token"] = $token;
+
+        if($redirect) {
+            // Redireciona para o perfil do usuário
+            $this->message->setMessage("Seja bem-vindo!", "sucess", "editprofile.php");
+        }
     }
+    
     public function authenticateUser($email, $password)
     {
     }
+    
     public function findByEmail($email)
     {
         if($email != "") {
@@ -69,12 +99,15 @@ class UserDAO implements UserDAOInterface
             return false;
         }
     }
+    
     public function findById($id)
     {
     }
+    
     public function findByToken($token)
     {
     }
+    
     public function changePassword(User $user)
     {
     }

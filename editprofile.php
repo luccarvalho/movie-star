ee4294594af66750b908a2a8c8e6202086569a48
Lucas <lucasalexandre_82@hotmail.com>
2022-12-03 11:10:28 -0300

    Finalizando o formulário de edição de usuário.

--- editprofile.php
+++ editprofile.php
@@ -12,11 +12,15 @@
 
     $fullName = $user->getFullName($userData);
 
+    if($userData->image == "") {
+        $userData->image = "user.png";
+    }
+
 ?>
 
 <div id="main-container" class="container-fluid">
     <div class="col-md-12">
-        <form action="<?= $BASE_URL ?>user_process.php" method="POST">
+        <form action="<?= $BASE_URL ?>user_process.php" method="POST" enctype="multipart/form-data">
             <input type="hidden" name="type" value="update">
             <div class="row">
                 <div class="col-md-4">
@@ -25,6 +29,26 @@
                     <div class="form-group">
                         <label for="name">Nome:</label>
                         <input type="text" class="form-control" id="name" name="name" placeholder="Digite o seu nome" value="<?= $userData->name ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="lastname">Sobrenome:</label>
+                        <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Digite o seu nome" value="<?= $userData->lastname ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">E-mail:</label>
+                        <input type="text" readonly class="form-control disabled" id="email" name="email" placeholder="Digite o seu nome" value="<?= $userData->email ?>">
+                    </div>
+                    <input type="submit" class="btn form-btn" value="Alterar">
+                </div>
+                <div class="col-md-4">
+                    <div id="profile-image-container" style="background-image: url('<?= $BASE_URL ?>img/users/<?= $userData->image ?>')"></div>
+                    <div class="form-group">
+                        <label for="image">Foto:</label>
+                        <input type="file" class="form-control-file" name="image">
+                    </div>
+                    <div class="form-group">
+                        <label for="bio">Sobre você:</label>
+                        <textarea class="form-control" name="bio" id="bio" rows="5" placeholder="Conte quem você é, o que faz e onde trabalha..."><?= $userData->bio ?></textarea>
                     </div>
                 </div>
             </div>

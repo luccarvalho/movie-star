652e74bcfa4b8962b3f09b8cc21c23e9646f0376
Lucas <lucasalexandre_82@hotmail.com>
2022-12-04 11:32:52 -0300

    Criação e estilização do formulário de edição de senha.

--- editprofile.php
+++ editprofile.php
@@ -18,7 +18,7 @@
 
 ?>
 
-<div id="main-container" class="container-fluid">
+<div id="main-container" class="container-fluid edit-profile-page">
     <div class="col-md-12">
         <form action="<?= $BASE_URL ?>user_process.php" method="POST" enctype="multipart/form-data">
             <input type="hidden" name="type" value="update">
@@ -38,7 +38,7 @@
                         <label for="email">E-mail:</label>
                         <input type="text" readonly class="form-control disabled" id="email" name="email" placeholder="Digite o seu nome" value="<?= $userData->email ?>">
                     </div>
-                    <input type="submit" class="btn form-btn" value="Alterar">
+                    <input type="submit" class="btn card-btn" value="Alterar">
                 </div>
                 <div class="col-md-4">
                     <div id="profile-image-container" style="background-image: url('<?= $BASE_URL ?>img/users/<?= $userData->image ?>')"></div>
@@ -53,6 +53,24 @@
                 </div>
             </div>
         </form>
+        <div class="row" id="change-password-container">
+            <div class="col-md-4">
+                <h2>Alterar a senha:</h2>
+                <p class="page-description">Digite a nova senha, depois confirme a alteração:</p>
+                <form action="<?= $BASE_URL ?>user_process.php" method="POST">
+                    <input type="hidden" name="type" value="changepassword">
+                    <div class="form-group">
+                        <label for="password">Nova senha:</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Digite a sua nova senha">
+                    </div>
+                    <div class="form-group">
+                        <label for="confirmpassword">Confirmação da senha:</label>
+                        <input type="password" class="form-control" id="confirmpassword" name="confirmpassword" placeholder="Confirme sua nova senha">
+                    </div>
+                    <input type="submit" class="btn card-btn" value="Alterar Senha">
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 

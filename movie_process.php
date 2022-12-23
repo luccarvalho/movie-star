22af6fafa2073323603b0a05a96f1a7f74c1bd6c
Lucas <lucasalexandre_82@hotmail.com>
2022-12-23 14:30:41 -0300

    Continuação do código para adição de filmes no banco de dados.

--- movie_process.php
+++ movie_process.php
@@ -27,6 +27,44 @@
 
         $movie = new Movie();
 
+        // Validação mínima de dados
+        if(!empty($title) && !empty($description) && !empty($category)) {
+
+            $movie->title = $title;
+            $movie->description = $description;
+            $movie->trailer = $trailer;
+            $movie->category = $category;
+            $movie->length = $length;
+
+            // Upload de imagem do filme
+            if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
+
+                $image = $_FILES["image"];
+                $imageTypes = ["image/jpeg", "image/jpg", "image/png"];
+
+                // Pegando extensão do arquivo
+                $ext = strtolower(substr($image["name"], -4));
+
+                // Checar se o arquivo é jpg
+                if($ext == ".jpg") {
+
+                    $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+
+                // Imagem é png
+                } else if($ext == ".png") {
+
+                    $imageFile = imagecreatefrompng($image["tmp_name"]);
+                }
+
+                $imageName = $movie->imageGenerateName($ext);
+            
+        }
+
+        } else {
+            
+            $message->setMessage("É necessário inserir ao menos o título, a descrição e a categoria!", "error", "back");
+        }
+
     } else {
         
         $message->setMessage("Informações inválidas!", "error", "index.php");

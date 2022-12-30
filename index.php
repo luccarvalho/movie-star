5cc077f1103550b7f4f01670ea306cf957328059
Lucas <lucasalexandre_82@hotmail.com>
2022-12-30 12:39:52 -0300

    Continuação do código para exibir os filmes na home.

--- index.php
+++ index.php
@@ -20,21 +20,8 @@
     <p class="section-description">Veja as críticas dos últimos filmes e séries adicionados no MovieStar</p>
     <div class="movies-container">
         <?php foreach($latestMovies as $movie) : ?>
-            <div class="card movie-card">
-                <div class="card-img-top" style="background-image: url('<?= $BASE_URL ?>img/movies/<?= $movie->image ?>')"></div>
-                <div class="card-body">
-                    <p class="card-rating">
-                        <i class="fas fa-star"></i>
-                        <span class="rating">9</span>
-                    </p>
-                    <h5 class="card-title">
-                        <a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>"><?= $movie->title ?></a>
-                    </h5>
-                    <a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>" class="btn btn-primary rate-btn">Avaliar</a>
-                    <a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>" class="btn btn-primary card-btn">Conhecer</a>
-                </div>
-            </div>
-        <?php endforeach; ?>    
+            <?php require("templates/movie_card.php"); ?>
+        <?php endforeach; ?>  
     </div>
     <h2 class="section-title">Ação</h2>
     <p class="section-description">Veja os melhores filmes e séries de ação</p>

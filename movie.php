c0896acca0a9574ac10ae51bfcf68ad719493ba9
Lucas <lucasalexandre_82@hotmail.com>
2023-01-09 17:14:28 -0300

    Estruturando a view de filmes.

--- movie.php
+++ movie.php
@@ -40,4 +40,25 @@
     }
 
     // Resgatar as revies do filme
-?>    
+?>
+
+<div id="main-container" class="container-fluid">
+    <div class="row">
+        <div class="offset-md-1 col-md-6 movie-container">
+            <h1 class="page-title"><?= $movie->title ?></h1>
+            <p class="movie-details">
+                <span>Duração: <?= $movie->length ?></span>
+                <span class="pipe"></span>
+                <span><?= $movie->category ?></span>
+                <span class="pipe"></span>
+                <span><i class="fas fa-star"></i> 9</span>
+            </p>
+            <iframe src="<?= $movie->trailer ?>" width="560" height="315" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encryted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
+<?php
+
+    require_once("templates/footer.php");
+
+?>

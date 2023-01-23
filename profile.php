af1495f5c6ec4ac44ddedb972c359627ffa4f5da
Lucas <lucasalexandre_82@hotmail.com>
2023-01-23 15:57:38 -0300

    Concluindo o HTML da página de visualização de perfis de usuários.

--- profile.php
+++ profile.php
@@ -9,6 +9,7 @@
 
     $user = new User();
     $userDao = new UserDAO($conn, $BASE_URL);
+    $movieDao = new MovieDAO($conn, $BASE_URL);
 
     // Receber id do usuário
     $id = filter_input(INPUT_GET, "id");
@@ -48,7 +49,31 @@
 
 ?>
 
-<div id="main-container" class="container-fluid"></div>
+<div id="main-container" class="container-fluid">
+    <div class="col-md-8 offset-md-2">
+        <div class="row profile-container">
+            <div class="col-md-12">
+                <h1 class="page-title"><?= $fullName ?></h1>
+                <div id="profile-image-container" style="background-image: url('<?= $BASE_URL ?>img/users/<?= $userData->image ?>')"></div>
+                <h3 class="about-title">Sobre:</h3>
+                <?php if(!empty($userData->bio)) : ?>
+                    <p class="profile-description"><?= $userData->bio ?></p>
+                <?php else: ?>
+                    <p class="profile-description">O usuário ainda não escreveu nada aqui...</p>
+                <?php endif; ?>    
+            </div>
+            <div class="col-md-12 added-movies-container">
+                <h3>Filmes ou séries que enviou:</h3>
+                <?php foreach($userMovies as $movie) : ?>
+                    <?php require("templates/movie_card.php"); ?>
+                <?php endforeach; ?>
+                <?php if(count($userMovies) === 0) : ?>
+                    <p class="empty-list">O usuário ainda não enviou filmes ou séries.</p>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
 
